17935e63227d0ac7a49a3b2fbc4634ee84426e64
Pusiaczek <lipek8@gmail.com>
2022-09-12 17:08:05 +0200

    inputs rearange

--- src/easy/inputs.js
+++ src/easy/inputs.js
@@ -1,11 +1,46 @@
-export const input_438 = [
-  2,
-  1,
-  10,
-  4,
-  5,
-  100,
-  7
+export const input_568 = [
+  "1 2 3 4 5",
+  "-100 100",
+  "2 5 4 6 8 9 7 8 5 6 3 2 1 5 4 7 8 96 3 5 12 6 8 4 56 58 4 2 32 54 9 52"
+]
+
+export const input_549 = [
+  "1 2 3 4 5",
+  "-100 100",
+  "2 5 4 6 8 9 7 8 5 6 3 2 1 5 4 7 8 96 3 5 12 6 8 4 56 58 4 2 32 54 9 52"
+]
+
+export const input_522 = [
+  [12, 15],
+  [11, 22],
+  [3, 6],
+  [1, 2],
+  [10, 100],
+  [25, 70],
+]
+
+export const input_506 = [
+  "AAAAAAAABBBBBBCCCCCCCSDASDASDAS",
+  "AABBCC",
+  "AAABBBCCC",
+  "AABBBCCCCDDDDDAABBAAABBB",
+  "ABCDEF",
+]
+
+export const input_499 = [
+  [2, 0],
+  [2, 3],
+  [2, 4],
+  [2, 5],
+  [2, 6],
+  [3, 5],
+  [7, 10],
+  [5, 5],
+  [5, 2],
+  [3, 3],
+  [12, 1232],
+  [6, 9],
+  [420, 1337],
 ]
 
 export const input_496 = [
@@ -29,41 +64,23 @@ export const input_496 = [
   100
 ]
 
-export const input_499 = [
-  [2, 0],
-  [2, 3],
-  [2, 4],
-  [2, 5],
-  [2, 6],
-  [3, 5],
-  [7, 10],
-  [5, 5],
-  [5, 2],
-  [3, 3],
-  [12, 1232],
-  [6, 9],
-  [420, 1337],
+export const input_438 = [
+  2,
+  1,
+  10,
+  4,
+  5,
+  100,
+  7
 ]
 
-export const input_506 = [
-  "AAAAAAAABBBBBBCCCCCCCSDASDASDAS",
-  "AABBCC",
-  "AAABBBCCC",
-  "AABBBCCCCDDDDDAABBAAABBB",
-  "ABCDEF",
-]
 
-export const input_522 = [
-  [12, 15],
-  [11, 22],
-  [3, 6],
-  [1, 2],
-  [10, 100],
-  [25, 70],
-]
 
-export const input_549 = [
-  "1 2 3 4 5",
-  "-100 100",
-  "2 5 4 6 8 9 7 8 5 6 3 2 1 5 4 7 8 96 3 5 12 6 8 4 56 58 4 2 32 54 9 52"
-]
+
+
+
+
+
+
+
+
